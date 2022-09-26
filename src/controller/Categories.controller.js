2f7a388bb5cf57022c7dabc30a32c6ac769517ae
costalucs <lucasdeslz@hotmail.com>
2022-09-26 14:50:19 -0300

    feat: category controller

--- src/controller/Categories.controller.js
+++ src/controller/Categories.controller.js
@@ -6,4 +6,13 @@ const getAllCategories = async (req, res) => {
   return res.status(200).json(allCategories);
 };
 
-module.exports = { getAllCategories };
+const postCategory = async (req, res) => {
+  const category = req.body;
+  if (!category.name) {
+ return res.status(400).json({ message: '"name" is required' }); 
+}
+  const newCategory = await CategoriesServices.insertCategory(category);
+  return res.status(201).json(newCategory);
+};
+
+module.exports = { getAllCategories, postCategory };
